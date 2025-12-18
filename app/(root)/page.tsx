a68fbd7b6dffd36454202c24dcd4d2656adcfa6f
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-19 01:21:37 +0200

    Implement hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -9,6 +16,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in virtual
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
