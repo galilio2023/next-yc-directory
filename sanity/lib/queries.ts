57f521f0abc130e91e8e19fa11a8d2365ca679e6
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-21 06:12:17 +0200

    bug fixs

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,8 +1,8 @@
 import { defineQuery } from "groq";
 
 export const STARTUPS_QUERY = defineQuery(`
-*[_type =='startup' && 
-  defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
+*[(_type =='startup' && 
+  defined(slug.current) && !defined($search)) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
   _id,
     slug,
     title,
