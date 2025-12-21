46b7d357e9b47db19d0c3efd83b233a693da267e
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-21 21:55:37 +0200

    Implement create-startup-page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,7 +12,6 @@ export default async function Home({
   const query = (await searchParams).query;
   const params = { search: query || null };
   const session = await auth();
-  console.log(session?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
   return (
